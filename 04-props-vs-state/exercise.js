6aeccb987f50d70db21af9969e0ac15ee1d0e9d3
NFabrizio <nick.fabrizio@pearson.com>
2019-10-15 22:43:56 -0600

    Removes confusing code from state vs props exercise file

--- 04-props-vs-state/exercise.js
+++ 04-props-vs-state/exercise.js
@@ -9,60 +9,7 @@
  * in your tech summit app and then, uncomment them.
  **/
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, Pearson!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
-}
 
-/* We can create a Clock component by pulling out and encapsulating the UI portion */
-
-// function Clock(props) {
-//   return (
-//     <div>
-//       <h1>Hello, world!</h1>
-//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-// }
-//
-// function tick() {
-//   ReactDOM.render(
-//     <Clock date={new Date()} />,
-//     document.getElementById('root')
-//   );
-// }
-
-setInterval(tick, 1000);
-
-/* Let's convert the Clock component to a classical component and implement state */
-
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       date: new Date()
-//     };
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello, world!</h1>
-//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-//       </div>
-//     );
-//   }
-// }
-//
-// ReactDOM.render(
-//   <Clock />,
-//   document.getElementById('root')
-// );
 
 
 /* CSS styling for button */
